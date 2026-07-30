db893cec1421cf87a2d2bdeaa0df053d12aa2e21
YuriiFreitass <yurifreitas1501@gmail.com>
2026-07-30 12:53:07 -0300

    feat: adiciona NotificacaoEntity e StatusNotificacao

---
+++ src/main/java/com/yurifreitas/notificacao_api/entity/NotificacaoEntity.java
@@ -0,0 +1,31 @@
+package com.yurifreitas.notificacao_api.entity;
+
+import com.yurifreitas.notificacao_api.enums.StatusNotificacao;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class NotificacaoEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private Long usuarioId;
+
+	private String mensagem;
+
+	@Enumerated(EnumType.STRING)
+	private StatusNotificacao status;
+
+	private LocalDateTime dataCriacao;
+}
---
+++ src/main/java/com/yurifreitas/notificacao_api/enums/StatusNotificacao.java
@@ -0,0 +1,10 @@
+package com.yurifreitas.notificacao_api.enums;
+
+import org.springframework.stereotype.Component;
+
+
+public enum StatusNotificacao {
+	PENDENTE,
+	ENVIADA,
+	FALHA
+}
